1274c9d31acf2f25d362139e1e6eef23886d37d7
Litty Mathew <littymathew3008@gmail.com>
2022-10-26 16:50:30 +0100

    date format changed

--- data-raw/climate.R
+++ data-raw/climate.R
@@ -2,5 +2,10 @@
 climate_raw<-readr::read_csv("data-raw/Climate.csv")
 head(climate_raw)
 Climate<-climate_raw
+library(dplyr)
+Climate <- Climate %>% 
+  mutate(DATE = as.Date(DATE, format = "%d-%b-%y"))
 Climate<-climate_raw[!apply(climate_raw == "", 1, all),]
 usethis::use_data(Climate,overwrite = T)
+
+
