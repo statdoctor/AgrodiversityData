1ed4ef59929f38144b2207a8e23afc44cee7a95a
Litty Mathew <littymathew3008@gmail.com>
2022-10-27 16:30:55 +0100

    date format

--- data-raw/climate.R
+++ data-raw/climate.R
@@ -1,11 +1,11 @@
 ## code to prepare `DATASET` dataset goes here
 climate_raw<-readr::read_csv("data-raw/Climate.csv")
 head(climate_raw)
-Climate<-climate_raw
 library(dplyr)
-Climate <- Climate %>% 
+climate_raw<-climate_raw%>% 
   mutate(DATE = as.Date(DATE, format = "%d-%b-%y"))
 Climate<-climate_raw[!apply(climate_raw == "", 1, all),]
+
 usethis::use_data(Climate,overwrite = T)
 usethis::use_r("Climate")
 
