d11febe4cd6781bc66d84b266aef38e673f55b28
Litty Mathew <littymathew3008@gmail.com>
2022-12-05 09:16:54 +0000

    biosum data and documentation changes in dec 5

--- R/Biosum.R
+++ R/Biosum.R
@@ -1,17 +1,26 @@
-#' Yearwise Yield data of the Agrodiversity experiment
+#' Year wise Yield data of the Agrodiversity experiment
 #'
 #'The \code{Biosum} dataset provides information about Species-specific biomass measurements (yield per species and of weeds) at 33 sites summarized by year.
-#' @format A dataframe with 92 rows and 9 variables:
+#' @format A dataframe with 3828 rows and 18 variables:
 #' \describe{
 #'   \item{SITE}{Site ID number}
 #'   \item{Country}{Country}
 #'   \item{Year}{Year}
-#'   \item{G1_YA}{Average harvest Dry Matter Yield per year of fast-establishing grass}
-#'   \item{G2_YA}{Average harvest Dry Matter Yield per year of persistent grass}
-#'   \item{L1_YA}{Average harvest Dry Matter Yield per year of fast-establishing legume }
-#'   \item{L2_YA}{Average harvest Dry Matter Yield per year of persistent legume }
-#'   \item{WEED_YA}{Average harvest Dry Matter Yield per year of weed species }
-#'   \item{HARV_YA}{Average harvest Dry Matter Yield per year}
+#'   \item{YEARN}{Experimental year number}
+#'   \item{NH}{Number of harvests – number of times the whole plot was cut in a year}
+#'   \item{PLOT}{Plot number as per design (1–30 = core design; 31–48 = treatment plots; 49–68 = additional plots at sites 45 and 46)}
+#'   \item{G1}{Initial sown proportion of fast-establishing grass}
+#'   \item{G2}{Initial sown proportion of persistent grass}
+#'   \item{L1}{Initial sown proportion of fast-establishing legume}
+#'   \item{L2}{Initial sown proportion of persistent legume}
+#'   \item{E}{Initial sown evenness}
+#'   \item{DENS}{Indicator variable: high=high level of initial sown biomass, low = low level (60%of high)}
+#'   \item{G1_YN}{Total harvest Dry Matter Yield per year of fast-establishing grass}
+#'   \item{G2_YN}{Total harvest Dry Matter Yield per year of persistent grass}
+#'   \item{L1_YN}{Total harvest Dry Matter Yield per year of fast-establishing legume }
+#'   \item{L2_YN}{Total harvest Dry Matter Yield per year of persistent legume }
+#'   \item{WEED_YN}{Total harvest Dry Matter Yield per year of weed species }
+#'   \item{HARV_YIELD2}{Total harvest Dry Matter Yield per year}
 #'}
 #' @source 
 #' Laura Kirwan, John Connolly, Caroline Brophy, Ole Baadshaug, Gilles Belanger, Alistair Black, Tim Carnus, Rosemary Collins, Jure Čop, Ignacio Delgado, Alex De Vliegher, Anjo Elgersma, Bodil Frankow-Lindberg, Piotr Golinski, Philippe Grieu, Anne-Maj Gustavsson, Áslaug Helgadóttir, Mats Höglind, Olivier Huguenin-Elie, Marit Jørgensen, Žydrė Kadžiulienė, Tor Lunnan, Andreas Lüscher, Päivi Kurki, Claudio Porqueddu, M.-Teresa Sebastia, Ulrich Thumm, David Walmsley, John Finn. 2014. The Agrodiversity Experiment: three years of data from a multisite study in intensively managed grasslands. Ecology, 95(9), pp. 2680-2680.
--- data-raw/Biosum.R
+++ data-raw/Biosum.R
@@ -1,13 +1,88 @@
-## code to prepare `biosum` dataset goes here
+## code to prepare `Biosum` dataset goes here
 
 rm(list=ls())
 biomass<-readr::read_csv("data-raw/Biomass.csv")
-head(biomass)
+View(biomass)
 library(dplyr)
-biosum_raw<-biomass%>%group_by(SITE,COUNTRY,YEAR)%>%summarise(across(c(G1_Y,G2_Y,L1_Y,L2_Y,WEED_Y,HARV_YIELD),mean))%>%as.data.frame()
-head(biosum_raw)
-biosum_raw<-biosum_raw%>%rename("G1_YA"="G1_Y", "G2_YA"="G2_Y", "L1_YA"="L1_Y", "L2_YA"="L2_Y", "WEED_YA"="WEED_Y","HARV_YA"="HARV_YIELD" )
-Biosum = biosum_raw
+attach(biomass)
+
+# count the Site wise missing value in all variables
+data_count2 <- biomass %>%                                   
+  group_by(SITE) %>%
+  summarize_each(funs(sum(is.na(.))))
+View(data_count2) 
+
+#Create duplicate HARV_Yield to average the actual harv yield if individual yield is missing in some harvest.
+bio<-biomass
+bio$HARV_YIELD2<-bio$HARV_YIELD
+View(bio)
+
+# Replace by harv yield=NA if WEED_Y=NA 
+bio1<-bio%>%mutate(HARV_YIELD = ifelse(is.na(WEED_Y), NA, HARV_YIELD)) 
+View(bio1)
+
+
+#Create a new variables avg of each yield group by (site, year, plot, na.rm=T)
+bio2<-bio1%>% group_by(SITE,YEAR,PLOT)%>%mutate(G1_Ya=mean(G1_Y,na.rm = T))
+bio2<-bio2%>% group_by(SITE,YEAR,PLOT)%>%mutate(G2_Ya=mean(G2_Y,na.rm = T))
+bio2<-bio2%>% group_by(SITE,YEAR,PLOT)%>%mutate(L1_Ya=mean(L1_Y,na.rm = T))
+bio2<-bio2%>% group_by(SITE,YEAR,PLOT)%>%mutate(L2_Ya=mean(L2_Y,na.rm = T))
+bio2<-bio2%>% group_by(SITE,YEAR,PLOT)%>%mutate(WEED_Ya=mean(WEED_Y,na.rm = T))
+bio2<-bio2%>% group_by(SITE,YEAR,PLOT)%>%mutate(HARV_Ya=mean(HARV_YIELD,na.rm = T))
+View(bio2)
+
+# create new proportions,avg of individual yield/Total average
+bio3<-bio2%>%mutate(G1_Yp=(G1_Ya/HARV_Ya))
+bio3<-bio3%>%mutate(G2_Yp=(G2_Ya/HARV_Ya))
+bio3<-bio3%>%mutate(L1_Yp=(L1_Ya/HARV_Ya))
+bio3<-bio3%>%mutate(L2_Yp=(L2_Ya/HARV_Ya))
+bio3<-bio3%>%mutate(WEED_Yp=(WEED_Ya/HARV_Ya))
+View(bio3)
+
+############ Create new variable to identify what happened
+# Now multiply each proportion with total yield for the missing rows (where all individual yield missing and total not missing) 
+bio4 <- bio3 %>% group_by(SITE,YEAR,PLOT) %>% mutate(G1_YN= ifelse(is.na(G1_Y),(G1_Yp*HARV_YIELD2),G1_Y))
+bio4 <- bio4 %>% group_by(SITE,YEAR,PLOT) %>% mutate(G2_YN= ifelse(is.na(G2_Y),(G2_Yp*HARV_YIELD2),G2_Y))
+bio4 <- bio4%>% group_by(SITE,YEAR,PLOT) %>% mutate(L1_YN= ifelse(is.na(L1_Y),(L1_Yp*HARV_YIELD2),L1_Y))
+bio4 <- bio4 %>% group_by(SITE,YEAR,PLOT) %>% mutate(L2_YN= ifelse(is.na(L2_Y),(L2_Yp*HARV_YIELD2),L2_Y))
+bio4 <- bio4 %>% group_by(SITE,YEAR,PLOT) %>% mutate(WEED_YN= ifelse(is.na(WEED_Y),(WEED_Yp*HARV_YIELD2),WEED_Y))
+View(bio4)
+# Create the new data frame with new variables
+bio5<-bio4%>%select(SITE:DENS,G1_YN,G2_YN,L1_YN,L2_YN,WEED_YN,HARV_YIELD2)
+View(bio5)# This is some what complete data
+
+# count the Site wise missing value in all variables
+data_count3 <- bio5 %>%                                   
+  group_by(SITE) %>%
+  summarize_each(funs(sum(is.na(.))))
+View(data_count3) 
+
+############################################################################
+# Now we can use bio5 data to make the biosum dataset
+
+# Sum the harvest by year per plot for each site
+bio6<-bio5%>%group_by(SITE,YEAR,PLOT)%>%summarise(across(c(G1_YN,G2_YN,L1_YN,L2_YN,WEED_YN,HARV_YIELD2),sum))%>%as.data.frame()
+dim(bio6)
+View(bio6)
+# Select intional variables from bio5 to attach it to the sum dataset
+bio7<-bio5%>%select(SITE:DENS,-c(HARVEST,HARVEST_DATE,TREAT,REP))
+View(bio7)
+
+bio8<-unique(bio7)
+dim(bio8)
+View(bio8)
+
+bio9<-left_join(bio8,bio6)
+View(bio9)# Final merged dataset
+
+# count the Site wise missing value in all variables
+data_count4 <- bio9 %>%                                   
+  group_by(SITE) %>%
+  summarize_each(funs(sum(is.na(.))))
+View(data_count4) 
+#####################################
+
+Biosum = bio9
 write.csv(Biosum,"data-raw/Biosum.csv")
 usethis::use_data(Biosum,overwrite = T)
 usethis::use_r("Biosum")
